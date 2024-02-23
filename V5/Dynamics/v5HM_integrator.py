ef22fc65381901618210cf248887db3dc43a2986
siddharth-mahesh <55521093+siddharth-mahesh@users.noreply.github.com>
2024-02-23 12:23:55 -0500

    Calibration + Final Trajectory
    Updates:
    
    - Hamiltonian and Derivative notebooks with a_6 and d_{\rm SO} as free parameters for calibration
    
    - Waveforms function for the full complex waveform
    
    - pygsl based integrator for the dynamics
    
    - auxiliary functions to process dynamics

---
+++ V5/Dynamics/v5HM_integrator.py
@@ -0,0 +1,84 @@
+import numpy as np
+from scipy.interpolate import CubicSpline
+from Dynamics.v5HM_Equations_Of_Motion import v5HM_unoptimized_rhs
+from Dynamics.v5HM_Initial_Conditions import v5HM_initial_conditions 
+import pygsl_lite.errno as errno
+import pygsl_lite.odeiv2 as odeiv2
+def v5HM_integrator(M,q,chi1,chi2,f):
+     
+    m1,m2,y_init,Omega0,h,rstop,risco,Deltat,af,Mf,h22NR,omega22NR = v5HM_unoptimized_initial_conditions(M,q,chi1,chi2,f) 
+     
+    sys = odeiv2.system(v5HM_unoptimized_RHS,None,4,[m1,m2,chi1,chi2]) 
+    T = odeiv2.step_rk8pd 
+    s = odeiv2.pygsl_lite_odeiv2_step(T,4) 
+    atol = 1e-11 
+    rtol = 1e-12 
+    c = odeiv2.pygsl_lite_odeiv2_control(atol,rtol,a_y = 1,a_dydt = 1,None) 
+    e = odeiv2.pygsl_lite_odeiv2_evolve(4) 
+     
+    prims = [] 
+    times = [] 
+    omega_previous = Omega_0 
+    omega_peak = False 
+    prstar_peak = False 
+    times.append(0.) 
+    prims.append(y_init) 
+     
+    while t < 2.0e9: 
+        status, t, h, y = e.apply(c,s,sys,t,t1,h,y) 
+        if status != errno.GSL_SUCCESS: 
+                print("break status", status) 
+                break 
+        prims.append(y) 
+        times.append(t) 
+     
+     
+        if r < = 6: 
+            rhs = v5HM_unoptimized_RHS(t,y,m1,m2,chi1,chi2) 
+            drdt = rhs[0] 
+            dphidt = rhs[1] 
+            dprstardt = rhs[2] 
+            if dphidt < omega_previous: 
+                    omega_peak = True 
+                    break 
+            if drdt > 0: 
+                    break 
+            if dprdt > 0: 
+                prstar_peak = True 
+                break 
+            if r < rstop: 
+                break 
+            if r < 3: 
+                phidot_circ = v5HM_unoptimized_circular_omega(m1,m2,r,pphi,chi1,chi2) 
+                if phidot_circ > 1: 
+                    break 
+            omega_previous = omega 
+     
+    t = np.array(times) 
+    dynamics = np.array(prims) 
+    step_back_time = 250. 
+    if omega_peak: 
+        t_desired = t[-1] - step_back_time - 50 
+    else: 
+        t_desired = t[-1] - step_back_time 
+     
+    coarse_fine_separation_idx = np.argmin(np.abs(t - t_desired)) 
+     
+    dynamics_coarse = np.c_(t[:coarse_fine_separation_idx],dynamics[:coarse_fine_separation_idx]) 
+    dynamics_fine_prelim = np.c_(t[coarse_fine_separation_idx:],dynamics[coarse_fine_separation_idx:]) 
+     
+    dynamics_coarse = augment_dynamics(dynamics_coarse,m1,m2,chi1,chi2) 
+    dynamics_fine_prelim = augment_dynamics(dynamics_fine,m1,m2,chi1,chi2) 
+     
+    t_peak = None 
+     
+    if omega_peak: 
+        interpolant = CubicSpline(dynamics_fine_prelim[:,0],dynamics_fine_prelim[:,6]) 
+        t_peak = iterative_refinement(interpolant.derivative(),[dynamics_fine_prelim[0,0],dynamics_fine[-1,0]]) 
+    if pr_peak: 
+        interpolant = CubicSpline(dynamics_fine_prelim[:,0],dynamics_fine_prelim[:,3]) 
+        t_peak = iterative_refinement(interpolant.derivative(),[dynamics_fine_prelim[-1,0] - 10, dynamics_fine[-1,0]]) 
+     
+    dynamics_fine = interpolate_dynamics(dynamics_fine_prelim[:,:5],omega_peak,step_back_time) 
+    dynamics_fine = augment_dynamics(dynamics_fine,m1,m2,chi1,chi2) 
+    return dynamics_coarse, dynamics_fine
---
+++ V5/Dynamics/v5HM_unoptimized_auxiliary_functions.py
@@ -0,0 +1,60 @@
+
+import numpy as np
+from scipy.signal import argrelmin
+from scipy.interpolate import CubicSpline
+from Hamiltonian.v5HM_unoptimized_hamiltonian import v5HM_unoptimized_hamiltonian as H
+from Derivatives.v5HM_Hamiltonian_Derivatives_unoptimized import v5HM_unoptimized_dH_dpphi as omega
+from Derivatives.v5HM_Hamiltonian_Derivatives_unoptimized import v5HM_unoptimized_omega_circ as omega_circ
+
+
+def augment_dynamics(dynamics, m1, m2, chi1, chi2):
+    result = []
+    eta = m1*m2/(m1 + m2)/(m1 + m2)
+    N = dynamics.shape[0]
+    for i in range(N):
+        dyn = dynamics[i]
+        Hreal = H(m1,m2,dyn[1],dyn[3],dyn[4],chi1,chi2)
+        Omega = omega(m1,m2,dyn[1],dyn[3],dyn[4],chi1,chi2)/eta
+        Omega_c = omega_circ(m1,m2,dyn[1],dyn[4],chi1,chi2)/eta
+        result.append([Hreal,Omega,Omega_c])
+    result = np.array(result)
+    return np.c_[dynamics,result]
+
+def iterative_refinement(var_dot, interval, peak_pr = False):
+    left = interval[0]
+    right = interval[1]
+    dt = 0.1
+    for n in range(2):
+        dt /= 10
+        t_array_fine = np.arange(interval[0],interval[1],dt)
+        abs_var_dot = np.abs(var_dot)
+        minima = argrelmin(abs_var_dot, order = 3)[0]
+        
+        if len(minima) > 0:
+            result = t_array_fine(minima[0])
+            interval = [ max(left,result - 10*dt) , min(right,result + 10*dt)]
+        else:
+            if peak_pr:
+                return interval[-1]
+            else:
+                return ( interval[0] + interval[-1] ) / 2
+    return result    
+
+def interpolate_dynamics(dynamics_fine, omega_peak = None, step_back = 250):
+    res = []
+    n = len(dyn_fine)
+    
+    if omega_peak:
+        t_start_fine = max(omega_peak - step_back, dynamics_fine[0,0])
+        t_new = np.arange(t_start, peak_omega, 0.1)
+    else:
+        t_new = np.arange(dynamics_fine[0,0], dynamics_fine[-1,0], 0.1)
+    
+    for i in range(1, dynamics_fine.shape[1]):
+        interpolant = CubicSpline(dynamics_fine[:,0], dynamics_fine[:,1])
+        res.append(interpolant(t_new))
+    
+    res = np.array(res)
+    res_transprosed = res.T
+    
+    return np.c_[t_new,res_transposed]
