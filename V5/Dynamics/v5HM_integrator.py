8020d062449104858849bd8fca2cf5b4b4704400
siddharth-mahesh <55521093+siddharth-mahesh@users.noreply.github.com>
2024-02-28 10:28:00 -0500

    more error fixes

--- V5/Dynamics/v5HM_integrator.py
+++ V5/Dynamics/v5HM_integrator.py
@@ -43,7 +43,7 @@ def v5HM_integrator(M,q,chi1,chi2,f):
      
         r = y[0] 
         if r <= 6: 
-            rhs = v5HM_unoptimized_RHS(t,y,m1,m2,chi1,chi2) 
+            rhs = v5HM_unoptimized_rhs(t,y,m1,m2,chi1,chi2) 
             drdt = rhs[0] 
             dphidt = rhs[1] 
             dprstardt = rhs[2] 
