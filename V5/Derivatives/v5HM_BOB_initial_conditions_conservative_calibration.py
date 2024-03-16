fc8e8ca39a8d65185b617581ba70fdd3d364cd26
siddharth-mahesh <55521093+siddharth-mahesh@users.noreply.github.com>
2024-03-15 21:54:14 -0400

    more import error fixes

--- V5/Derivatives/v5HM_BOB_initial_conditions_conservative_calibration.py
+++ V5/Derivatives/v5HM_BOB_initial_conditions_conservative_calibration.py
@@ -1,6 +1,6 @@
 import numpy as np
-from Derivatives.v5HM_BOB_unoptimized_hamiltonian_derivatives_calibration import v5HM_BOB_unoptimized_omega_circ_calibration as dHdpphi_circ
-from Derivatives.v5HM_BOB_unoptimized_hamiltonian_derivatives_calibration import v5HM_BOB_unoptimized_dH_dr_circ_calibration as dHdr_circ
+from Derivatives.v5HM_BOB_unoptimized_hamiltonian_first_derivatives_calibration import v5HM_BOB_unoptimized_omega_circ_calibration as dHdpphi_circ
+from Derivatives.v5HM_BOB_unoptimized_hamiltonian_first_derivatives_calibration import v5HM_BOB_unoptimized_dH_dr_circ_calibration as dHdr_circ
 def v5HM_BOB_unoptimized_IC_cons_calibration(u , params):
     m1, m2, chi1, chi2, a6, dSO, omega = params[0], params[1], params[2], params[3], params[4], params[5], params[6]
     r, pphi = u[0], u[1]
