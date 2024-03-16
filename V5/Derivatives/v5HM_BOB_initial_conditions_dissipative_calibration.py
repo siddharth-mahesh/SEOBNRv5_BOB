0da57a2000b86aa4d0cb0c7c865fbc82cbd9b341
siddharth-mahesh <55521093+siddharth-mahesh@users.noreply.github.com>
2024-03-15 21:56:26 -0400

    more function name changes

--- V5/Derivatives/v5HM_BOB_initial_conditions_dissipative_calibration.py
+++ V5/Derivatives/v5HM_BOB_initial_conditions_dissipative_calibration.py
@@ -6,7 +6,7 @@ from Derivatives.v5HM_BOB_unoptimized_hamiltonian_first_derivatives_calibration 
 from Derivatives.v5HM_BOB_unoptimized_hamiltonian_first_derivatives_calibration import v5HM_BOB_unoptimized_dH_dpphi_calibration as omega
 from Derivatives.v5HM_BOB_unoptimized_hamiltonian_first_derivatives_calibration import v5HM_BOB_unoptimized_dH_dprstar_calibration as dHdprstar
 from Hamiltonian.v5HM_BOB_unoptimized_hamiltonian_calibration import v5HM_BOB_unoptimized_hamiltonian_calibration as H
-def v5HM_unoptimized_calibration_IC_diss(prstar, params):
+def v5HM_unoptimized_IC_diss_calibration(prstar, params):
     m1, m2, r, pphi, chi1, chi2, a6, dSO = params[0], params[1], params[2], params[3], params[4], params[5], params[6], params[7]
     M = m1 + m2
     eta = m1*m2/M/M
